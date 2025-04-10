b4d3485096b26f5786bb36272fc2729a21a353dc
Jihyun Cho <senscho@icloud.com>
2025-04-09 23:36:30 -0700

    make hotels/slug more cardlike

--- src/app/hotels/[slug]/page.tsx
+++ src/app/hotels/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { getHotelBySlug, getHotels } from '@/lib/strapi';
+import { getHotelBySlug } from '@/lib/strapi';
 import { notFound } from 'next/navigation';
 import { HotelCard } from '@/app/components/hotel/HotelCard';
 
@@ -8,15 +8,15 @@ interface HotelPageProps {
   };
 }
 
-export async function generateStaticParams() {
-  const hotels = await getHotels();
-  return hotels.map((hotel) => ({
-    slug: hotel.slug,
-  }));
-}
+// export async function generateStaticParams() {
+//   const hotels = await getHotels();
+//   return hotels.map((hotel) => ({
+//     slug: hotel.slug,
+//   }));
+// }
 
 export default async function HotelPage({ params }: HotelPageProps) {
-  const { slug } = await params;
+  const { slug } = params;
   const hotel = await getHotelBySlug(slug);
 
   if (!hotel) {
@@ -24,8 +24,11 @@ export default async function HotelPage({ params }: HotelPageProps) {
   }
 
   return (
-    <main className="container mx-auto px-4 py-8">
-      <HotelCard hotel={hotel} />
-    </main>
+    <div className="container mx-auto px-4 py-8">
+      <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {/* <main className="container mx-auto px-4 py-8"> */}
+        <HotelCard hotel={hotel} />
+      </main>
+    </div>
   );
 } 
